4a91bd0203546a321db6cc57ced5142480757f3f
asmithasatesh <asmithasatesh@gmail.com>
2021-07-09 17:38:13 +0530

    [Asmitha]Push data to stack

---
+++ Stack and queue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Stack_and_queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating object for stack and queue operations
+            StackAndQueueOperations stack = new StackAndQueueOperations();
+            
+            //Push operations
+
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            //Display operation
+            Console.Write("\n-----DISPLAYING STACK ELEMENTS-----\n\n");
+            stack.Display();
+        }
+    }
+}
